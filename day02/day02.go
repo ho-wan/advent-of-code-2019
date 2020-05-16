c1d4ee6997d2e15a8e9dc7bbd79c2737ee596a29
Ho-Wan <ho.wan2345@gmail.com>
2020-05-16 21:44:50 +0100

    day2 part2

--- day02/day02.go
+++ day02/day02.go
@@ -37,13 +37,17 @@ func readNums(path string) ([]int, error) {
 
 func runProgram(in []int) ([]int, error) {
 	nums := in
-	for i := 0; i < len(nums); i += 4 {
-		switch nums[i] {
-		case 99:
+	for i := 0; i <= len(nums); i += 4 {
+		if nums[i] == 99 {
 			break
-		case 1:
+		}
+		if i > len(nums)-4 {
+			return nums, errors.New("no solution, end of nums reached")
+		}
+		if nums[i] == 1 {
 			nums[nums[i+3]] = nums[nums[i+1]] + nums[nums[i+2]]
-		case 2:
+		}
+		if nums[i] == 2 {
 			nums[nums[i+3]] = nums[nums[i+1]] * nums[nums[i+2]]
 		}
 	}
@@ -55,10 +59,31 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	nums[1] = 12
+	nums[2] = 2
 
 	res, err := runProgram(nums)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("part1: %v", res[0])
+	fmt.Printf("part1: %v\n", res[0])
+
+	for i := 0; i < 99; i++ {
+		for j := 0; j < 99; j++ {
+			nums, err := readNums("day02-input.txt")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			nums[1] = i
+			nums[2] = j
+			res, err := runProgram(nums)
+			if err != nil {
+				fmt.Printf("error %v\n", err)
+			}
+			if res[0] == 19690720 {
+				fmt.Printf("part2: %v\n", 100*i+j)
+			}
+		}
+	}
 }
