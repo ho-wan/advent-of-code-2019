d1e36c4176510eea6f22ce8fc2130a91a346657e
Ho-Wan <ho.wan2345@gmail.com>
2020-05-19 01:18:35 +0100

    day3 part1
     Please enter the commit message for your changes. Lines starting

--- day03/day03.go
+++ day03/day03.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 Psuedocode
 - convert (w1,w2) to slice of strings
@@ -9,10 +19,133 @@ Psuedocode
 - get manhattan dist from each cross to origin, and return smaller
 */
 
-func getDist(w1, w2 string) int {
-	return 0
+var errConv = errors.New("cannot convert string to int")
+
+func strToVec(w1 string) ([][2]int, error) {
+	sw1 := strings.Split(w1, ",")
+	v := [][2]int{}
+	for _, s := range sw1 {
+		dir := s[:1]
+		n := s[1:]
+		dst, err := strconv.Atoi(n)
+		if err != nil {
+			return nil, fmt.Errorf("convertStringToInt: error: %w", errConv)
+		}
+
+		tv := [2]int{0, 0}
+		switch dir {
+		case "L":
+			tv[0] -= dst
+		case "R":
+			tv[0] += dst
+		case "D":
+			tv[1] -= dst
+		case "U":
+			tv[1] += dst
+		}
+		v = append(v, tv)
+	}
+	return v, nil
+}
+
+func getCoords(co [][2]int) [][2]int {
+	coords := [][2]int{}
+	pos := [2]int{0, 0}
+	for _, c := range co {
+		if c[0] == 0 && c[1] == 0 {
+			continue
+		}
+		for ; c[0] > 0; c[0]-- {
+			pos[0]++
+			coords = append(coords, pos)
+		}
+		for ; c[0] < 0; c[0]++ {
+			pos[0]--
+			coords = append(coords, pos)
+		}
+		for ; c[1] > 0; c[1]-- {
+			pos[1]++
+			coords = append(coords, pos)
+		}
+		for ; c[1] < 0; c[1]++ {
+			pos[1]--
+			coords = append(coords, pos)
+		}
+	}
+	return coords
+}
+
+func getDist(w1, w2 string) (int, error) {
+	vw1, err := strToVec(w1)
+	if err != nil {
+		return -1, fmt.Errorf("getDist: %w", err)
+	}
+	vw2, err := strToVec(w2)
+	if err != nil {
+		return -1, fmt.Errorf("getDist: %w", err)
+	}
+
+	cw1 := getCoords(vw1)
+	cw2 := getCoords(vw2)
+
+	overlap := [][2]int{}
+	for i, c1 := range cw1 {
+		for _, c2 := range cw2 {
+			if c1[0] == c2[0] && c1[1] == c2[1] {
+				overlap = append(overlap, c1)
+				continue
+			}
+		}
+		// for visual progress
+		if i%10000 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	var minMd int
+	for _, o := range overlap {
+		md := absInt(o[0]) + absInt(o[1])
+		if md < minMd || minMd == 0 {
+			minMd = md
+		}
+	}
+	return minMd, nil
+}
+
+func absInt(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, nil
 }
 
 func main() {
+	lines, err := readLines("day03-input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	log.Printf("got lines, length %d, %d", len(lines[0]), len(lines[1]))
+
+	p1, err := getDist(lines[0], lines[1])
+	if err != nil {
+		log.Fatalf("fatal error: %v", err)
+	}
+	fmt.Println("Part1: ", p1)
 }
--- day03/day03_test.go
+++ day03/day03_test.go
@@ -1,24 +1,75 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestMain(t *testing.T) {
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestStrToVec(t *testing.T) {
 	tt := []struct {
 		name string
-		w1   string
-		w2   string
-		expD int
+		in   string
+		wOut [][2]int
 	}{
-		{"case1", "R8,U5,L5,D3", "U7,R6,D4,L4", 6},
-		{"case2", "R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159},
-		{"case2", "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135},
+		{"c1", "R8,U5,L5,D3", [][2]int{{8, 0}, {0, 5}, {-5, 0}, {0, -3}}},
+		{"c2", "U7,R6,D4,L4", [][2]int{{0, 7}, {6, 0}, {0, -4}, {-4, 0}}},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			d := getDist(tc.w1, tc.w2)
-			if d != tc.expD {
-				t.Fatalf("got %v, want %v", d, tc.expD)
+			d, err := strToVec(tc.in)
+			if err != nil {
+				t.Fatalf("go unexpected error: %v", err)
+			}
+			if !cmp.Equal(d, tc.wOut) {
+				t.Fatalf("got %v, want %v", d, tc.wOut)
+			}
+		})
+	}
+
+}
+
+func TestGetCoords(t *testing.T) {
+	tt := []struct {
+		name string
+		in   [][2]int
+		wOut [][2]int
+	}{
+		{"c1", [][2]int{{2, 0}, {0, -1}}, [][2]int{{1, 0}, {2, 0}, {2, -1}}},
+		{"c1", [][2]int{{1, 0}, {0, 1}, {-2, 0}}, [][2]int{{1, 0}, {1, 1}, {0, 1}, {-1, 1}}},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			out := getCoords(tc.in)
+			if !cmp.Equal(out, tc.wOut) {
+				t.Fatalf("got %v, want %v", out, tc.wOut)
+			}
+		})
+	}
+}
+
+func TestGetDist(t *testing.T) {
+	tt := []struct {
+		name string
+		w1   string
+		w2   string
+		wOut int
+	}{
+		{"c1", "R8,U5,L5,D3", "U7,R6,D4,L4", 6},
+		{"c2", "R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159},
+		{"c3", "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			d, err := getDist(tc.w1, tc.w2)
+			if err != nil {
+				t.Fatalf("go unexpected error: %v", err)
+			}
+			if d != tc.wOut {
+				t.Fatalf("got %v, want %v", d, tc.wOut)
 			}
 		})
 	}
