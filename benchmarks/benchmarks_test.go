4b61e8f63f5124c9182986a8f1930d188de2f3f5
Ho-Wan <ho.wan2345@gmail.com>
2020-10-17 13:38:15 +0100

    add benchmarks for slice

---
+++ benchmarks/benchmarks_test.go
@@ -0,0 +1,23 @@
+package benchmarks
+
+import "testing"
+
+func BenchmarkSliceAppendNoCap(b *testing.B) {
+	var a []int
+	for i := 0; i < b.N; i++ {
+		a = append(a, i)
+	}
+}
+func BenchmarkSliceAppend(b *testing.B) {
+	a := make([]int, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		a = append(a, i)
+	}
+}
+
+func BenchmarkSliceSet(b *testing.B) {
+	a := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		a[i] = i
+	}
+}
